b083f6732c6f04b1839e949f55239b8261352675
Mateus <mateuskvasconcelos@gmail.com>
2023-02-08 17:55:43 -0300

    Feat: Method POST /customers done

--- src/middleware/customersMiddleware.js
+++ src/middleware/customersMiddleware.js
@@ -1,1 +1,17 @@
+import connection from "../database/database.js";
 
+export async function customersDocumentValidation(_, res, next) {
+  const { cpf } = res.locals;
+  try {
+    const result = await connection.query(
+      `SELECT * FROM customers WHERE (cpf) = '${cpf}';`
+    );
+    console.log(result);
+    if (result.rows.length === 1) {
+      return res.sendStatus(409);
+    }
+    next();
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+}
--- src/routers/customersRouter.js
+++ src/routers/customersRouter.js
@@ -4,10 +4,16 @@ import {
   customersRead,
   customersReadId,
 } from "../controllers/customersController.js";
+import { customersDocumentValidation } from "../middleware/customersMiddleware.js";
 import { customersValidation } from "../middleware/schemaMiddleware.js";
 
 const customersRouter = Router();
-customersRouter.post("/customers", customersValidation, customersCreate);
+customersRouter.post(
+  "/customers",
+  customersValidation,
+  customersDocumentValidation,
+  customersCreate
+);
 customersRouter.get("/customers", customersRead);
 customersRouter.get("/customers/:id", customersReadId);
 
