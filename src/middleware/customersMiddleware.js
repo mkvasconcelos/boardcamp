8183efae98439fe73516e122676acbc2b197281b
Mateus <mateuskvasconcelos@gmail.com>
2023-02-08 18:44:03 -0300

    Fix: Bug of id undefined corrected

--- src/middleware/customersMiddleware.js
+++ src/middleware/customersMiddleware.js
@@ -3,9 +3,13 @@ import connection from "../database/database.js";
 export async function customersDocumentValidation(_, res, next) {
   const { cpf, id } = res.locals;
   try {
-    const result = await connection.query(
-      `SELECT * FROM customers WHERE cpf = '${cpf}' AND id <> ${id};`
-    );
+    let query;
+    if (id) {
+      query = `SELECT * FROM customers WHERE cpf = '${cpf}' AND id <> ${id};`;
+    } else {
+      query = `SELECT * FROM customers WHERE cpf = '${cpf}';`;
+    }
+    const result = await connection.query(query);
     if (result.rows.length !== 0) {
       return res.sendStatus(409);
     }
