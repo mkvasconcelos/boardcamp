be44335a1da5050d6fb8d8ab0d2f187f05751b78
Mateus <mateuskvasconcelos@gmail.com>
2023-02-07 21:32:55 -0300

    Feat: Method GET/id /customers done

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -8,17 +8,32 @@ export async function customersCreate(_, res) {
          VALUES ($1, $2, $3, $4);`,
       [name, phone, cpf, birthday]
     );
-    res.send(201);
+    return res.send(201);
   } catch (err) {
-    res.status(500).send(err);
+    return res.status(500).send(err);
   }
 }
 
 export async function customersRead(_, res) {
   try {
     const result = await connection.query(`SELECT * FROM customers;`);
-    res.send(result.rows);
+    return res.status(200).send(result.rows);
   } catch (err) {
-    res.status(500).send(err);
+    return res.status(500).send(err);
+  }
+}
+
+export async function customersReadId(req, res) {
+  const { id } = req.params;
+  try {
+    const result = await connection.query(
+      `SELECT * FROM customers WHERE id = ${id};`
+    );
+    if (result.rows.length === 0) {
+      return res.sendStatus(404);
+    }
+    return res.status(200).send(result.rows);
+  } catch (err) {
+    return res.status(500).send(err);
   }
 }
--- src/routers/customersRouter.js
+++ src/routers/customersRouter.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   customersCreate,
   customersRead,
+  customersReadId,
 } from "../controllers/customersController.js";
 import { customersValidation } from "../middleware/schemaMiddleware.js";
 
 const customersRouter = Router();
 customersRouter.post("/customers", customersValidation, customersCreate);
 customersRouter.get("/customers", customersRead);
+customersRouter.get("/customers/:id", customersReadId);
 
 export default customersRouter;
